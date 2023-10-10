65e2024aefafa77f339bbb6951665e701c74cbcd
Liz Rurangwa <107972391+ruraliz@users.noreply.github.com>
2023-10-10 12:16:10 -0700

    created react app and added starter files

---
+++ react-gallery-app/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route, Routes, Navigate } from "react-router-dom";
+import { apikey } from './config'
+import './App.css'
+
+function App() {
+
+}
+
+export default App
